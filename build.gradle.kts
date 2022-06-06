30c906f531812e5cc56f308f521370d881353ee5
Alexander Brandes <mc.cache@web.de>
2022-06-06 11:25:45 +0200

    feat: Initial project structure

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+import java.net.URI
+
+plugins {
+    `maven-publish`
+    signing
+
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+
+    idea
+    eclipse
+}
+
+group = "com.intellectualsites"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+signing {
+    if (!version.toString().endsWith("-SNAPSHOT")) {
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        signing.isRequired
+        sign(publishing.publications)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+
+            pom {
+
+                name.set(project.name + " " + project.version)
+                description.set("Bill of materials for IntellectualSites projects")
+                url.set("https://github.com/IntellectualSites/bom")
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License, Version 3.0")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.html")
+                        distribution.set("repo")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("NotMyFault")
+                        name.set("Alexander Brandes")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/IntellectualSites/bom")
+                    connection.set("scm:https://IntellectualSites@github.com/IntellectualSites/bom.git")
+                    developerConnection.set("scm:git://github.com/IntellectualSites/bom.git")
+                }
+
+                issueManagement {
+                    system.set("GitHub")
+                    url.set("https://github.com/IntellectualSites/bom/issues")
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(URI.create("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(URI.create("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
+}
